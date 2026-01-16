34f226a77f9e9833e9521c808ebda7d45d53b74d
FILEXDZ <143833526+FILEXDZ@users.noreply.github.com>
2026-01-16 23:28:22 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    siteName: "YOUSSEF BOULANOUR",
+    siteLogo: "source/logo.png", 
+    socialLinks: {
+        youtube: "https://youtube.com/@YourChannel",
+        facebook: "https://facebook.com/YourProfile",
+        instagram: "https://instagram.com/YourProfile",
+        spotify: "https://open.spotify.com/user/YourProfile",
+        steam: "https://steamcommunity.com/id/YourID",
+        tiktok: "https://tiktok.com/@YourUsername"
+    }
+};
