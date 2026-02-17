d5c31707fbeac8946997ca1cf864e749c5a39bea
FILEXDZ <143833526+FILEXDZ@users.noreply.github.com>
2026-02-17 15:28:29 +0100

    Modify social links in config.js
    Updated social media links in the config file.

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ const config = {
     siteName: "YOUSSEF BOULANOUR",
     siteLogo: "source/logo.png", 
     socialLinks: {
-        youtube: "https://youtube.com/@YourChannel",
-        facebook: "https://facebook.com/YourProfile",
-        instagram: "https://instagram.com/YourProfile",
-        spotify: "https://open.spotify.com/user/YourProfile",
-        steam: "https://steamcommunity.com/id/YourID",
-        tiktok: "https://tiktok.com/@YourUsername"
+        //youtube: "https://youtube.com/@YourChannel",
+        facebook: "https://www.facebook.com/FILEXDZ",
+        instagram: "https://www.instagram.com/youssef_.bln",
+        spotify: "https://open.spotify.com/user/31tnstxrjg47fpdsvh442xpennvy?nd=1&dlsi=7fede851e7b2481a",
+        steam: "https://steamcommunity.com/id/FILEX-DZ/",
+        tiktok: "https://www.tiktok.com/@filexdzz"
     }
 };
